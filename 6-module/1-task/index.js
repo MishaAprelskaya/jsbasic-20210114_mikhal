9111a48efedf6eb9afcf16c2046df1850c63fff6
MishaAprelskaya <mikhal.aprelskaya@gmail.com>
2021-02-14 22:26:12 +0300

    module 6, task 1 and 2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -4,7 +4,7 @@ export default class UserTable {
 
 	constructor(rows) {
 		this.rows = rows;
-
+		console.log(this.rows);
 		this.elem = document.createElement("table");
 
 		let tbody = document.createElement("tbody");
@@ -17,7 +17,7 @@ export default class UserTable {
 
 			for (let item in rows[i]) {
 				let td = document.createElement("td");
-				td.innerHTML = rows[i].item;
+				td.innerHTML = rows[i][item];
 				tr.appendChild(td);
 			}
 
@@ -27,16 +27,13 @@ export default class UserTable {
 			let buttonX = td.lastChild;
 
 			tr.appendChild(td);
-
 			tbody.appendChild(tr);
-
-			function hideTr() {
-				tr.remove();
-			}
-
-			buttonX.addEventListener('click', hideTr);
+			buttonX.addEventListener('click', () => { this.removeRow(buttonX); });
 		}
 
 	}
-}
 
+	removeRow(btn) {
+		btn.closest('tr').remove();
+	}
+}
